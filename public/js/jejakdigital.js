80e242711e69a5fda5d516e5461aa460e24c63aa
haritsf <haritsf@hotmail.com>
2019-11-28 20:08:11 +0700

    Refine All Attr Prop Sesi 2

---
+++ public/js/jejakdigital.js
@@ -0,0 +1,146 @@
+    function LeftCBFacebook() {
+        if ($('#left-cb-facebook').prop('checked')==true) {
+            $('#left-facebook').attr('style', 'display:none');
+            $('#left-cb-facebook').attr('checked', 'false');
+            $('#right-facebook').attr('style', 'display:block');
+            $('#right-cb-facebook').attr('checked', 'true');
+        }else{
+            $('#left-facebook').attr('style', 'display:none');
+            $('#left-cb-facebook').attr('checked', 'false');
+            $('#right-facebook').attr('style', 'display:block');
+            $('#right-cb-facebook').attr('checked', 'true');
+        }
+    }
+    function RightCBFacebook() {
+        if ($('#right-cb-facebook').prop('checked')==false) {
+            $('#left-facebook').attr('style', 'display:block');
+            $('#left-cb-facebook').attr('checked', 'false');
+            $('#right-facebook').attr('style', 'display:none');
+        }
+    }
+
+    function LeftCBTwitter() {
+        if ($('#left-cb-twitter').prop('checked')==true) {
+            $('#left-twitter').attr('style', 'display:none');
+            $('#left-cb-twitter').attr('checked', 'false');
+            $('#right-twitter').attr('style', 'display:block');
+            $('#right-cb-twitter').attr('checked', 'true');
+        }else{
+            $('#left-twitter').attr('style', 'display:none');
+            $('#left-cb-twitter').attr('checked', 'false');
+            $('#right-twitter').attr('style', 'display:block');
+            $('#right-cb-twitter').attr('checked', 'true');
+        }
+    }
+    function RightCBTwitter() {
+        if ($('#right-cb-twitter').prop('checked')==false) {
+            $('#left-twitter').attr('style', 'display:block');
+            $('#left-cb-twitter').attr('checked', 'false');
+            $('#right-twitter').attr('style', 'display:none');
+        }
+    }
+
+    function LeftCBWhatsApp() {
+        if ($('#left-cb-whatsapp').prop('checked')==true) {
+            $('#left-whatsapp').attr('style', 'display:none');
+            $('#left-cb-whatsapp').attr('checked', 'false');
+            $('#right-whatsapp').attr('style', 'display:block');
+            $('#right-cb-whatsapp').attr('checked', 'true');
+        }else{
+            $('#left-whatsapp').attr('style', 'display:none');
+            $('#left-cb-whatsapp').attr('checked', 'false');
+            $('#right-whatsapp').attr('style', 'display:block');
+            $('#right-cb-whatsapp').attr('checked', 'true');
+        }
+    }
+    function RightCBWhatsApp() {
+        if ($('#right-cb-whatsapp').prop('checked')==false) {
+            $('#left-whatsapp').attr('style', 'display:block');
+            $('#left-cb-whatsapp').attr('checked', 'false');
+            $('#right-whatsapp').attr('style', 'display:none');
+        }
+    }
+
+    function LeftCBLINE() {
+        if ($('#left-cb-line').prop('checked')==true) {
+            $('#left-line').attr('style', 'display:none');
+            $('#left-cb-line').attr('checked', 'false');
+            $('#right-line').attr('style', 'display:block');
+            $('#right-cb-line').attr('checked', 'true');
+        }else{
+            $('#left-line').attr('style', 'display:none');
+            $('#left-cb-line').attr('checked', 'false');
+            $('#right-line').attr('style', 'display:block');
+            $('#right-cb-line').attr('checked', 'true');
+        }
+    }
+    function RightCBLINE() {
+        if ($('#right-cb-line').prop('checked')==false) {
+            $('#left-line').attr('style', 'display:block');
+            $('#left-cb-line').attr('checked', 'false');
+            $('#right-line').attr('style', 'display:none');
+        }
+    }
+
+    function LeftCBInstagram() {
+        if ($('#left-cb-instagram').prop('checked')==true) {
+            $('#left-instagram').attr('style', 'display:none');
+            $('#left-cb-instagram').attr('checked', 'false');
+            $('#right-instagram').attr('style', 'display:block');
+            $('#right-cb-instagram').attr('checked', 'true');
+        }else{
+            $('#left-instagram').attr('style', 'display:none');
+            $('#left-cb-instagram').attr('checked', 'false');
+            $('#right-instagram').attr('style', 'display:block');
+            $('#right-cb-instagram').attr('checked', 'true');
+        }
+    }
+    function RightCBInstagram() {
+        if ($('#right-cb-instagram').prop('checked')==false) {
+            $('#left-instagram').attr('style', 'display:block');
+            $('#left-cb-instagram').attr('checked', 'false');
+            $('#right-instagram').attr('style', 'display:none');
+        }
+    }
+
+    function LeftCBTelegram() {
+        if ($('#left-cb-telegram').prop('checked')==true) {
+            $('#left-telegram').attr('style', 'display:none');
+            $('#left-cb-telegram').attr('checked', 'false');
+            $('#right-telegram').attr('style', 'display:block');
+            $('#right-cb-telegram').attr('checked', 'true');
+        }else{
+            $('#left-telegram').attr('style', 'display:none');
+            $('#left-cb-telegram').attr('checked', 'false');
+            $('#right-telegram').attr('style', 'display:block');
+            $('#right-cb-telegram').attr('checked', 'true');
+        }
+    }
+    function RightCBTelegram() {
+        if ($('#right-cb-telegram').prop('checked')==false) {
+            $('#left-telegram').attr('style', 'display:block');
+            $('#left-cb-telegram').attr('checked', 'false');
+            $('#right-telegram').attr('style', 'display:none');
+        }
+    }
+
+    function LeftCBYouTube() {
+        if ($('#left-cb-youtube').prop('checked')==true) {
+            $('#left-youtube').attr('style', 'display:none');
+            $('#left-cb-youtube').attr('checked', 'false');
+            $('#right-youtube').attr('style', 'display:block');
+            $('#right-cb-youtube').attr('checked', 'true');
+        }else{
+            $('#left-youtube').attr('style', 'display:none');
+            $('#left-cb-youtube').attr('checked', 'false');
+            $('#right-youtube').attr('style', 'display:block');
+            $('#right-cb-youtube').attr('checked', 'true');
+        }
+    }
+    function RightCBYouTube() {
+        if ($('#right-cb-youtube').prop('checked')==false) {
+            $('#left-youtube').attr('style', 'display:block');
+            $('#left-cb-youtube').attr('checked', 'false');
+            $('#right-youtube').attr('style', 'display:none');
+        }
+    }
--- resources/views/user/jejakdigital.blade.php
+++ resources/views/user/jejakdigital.blade.php
@@ -83,11 +83,40 @@
                                             <span class="selectgroup-button">Facebook</span>
                                         </label>
                                     </div>
-
                                     <div id="left-twitter" class="selectgroup selectgroup-pills" style="display:block">
                                         <label class="selectgroup-item">
-                                            <input id="left-cb-twitter" type="checkbox" value="Mencintai Diri Sendiri" class="selectgroup-input" onclick="LeftCBTwitter()">
+                                            <input id="left-cb-twitter" type="checkbox" class="selectgroup-input" onclick="LeftCBTwitter()">
                                             <span class="selectgroup-button">Twitter</span>
+                                        </label>
+                                    </div>
+                                    <div id="left-whatsapp" class="selectgroup selectgroup-pills" style="display:block">
+                                        <label class="selectgroup-item">
+                                            <input id="left-cb-whatsapp" type="checkbox" class="selectgroup-input" onclick="LeftCBWhatsApp()">
+                                            <span class="selectgroup-button">WhatsApp</span>
+                                        </label>
+                                    </div>
+                                    <div id="left-line" class="selectgroup selectgroup-pills" style="display:block">
+                                        <label class="selectgroup-item">
+                                            <input id="left-cb-line" type="checkbox" class="selectgroup-input" onclick="LeftCBLINE()">
+                                            <span class="selectgroup-button">LINE</span>
+                                        </label>
+                                    </div>
+                                    <div id="left-instagram" class="selectgroup selectgroup-pills" style="display:block">
+                                        <label class="selectgroup-item">
+                                            <input id="left-cb-instagram" type="checkbox" class="selectgroup-input" onclick="LeftCBInstagram()">
+                                            <span class="selectgroup-button">Instagram</span>
+                                        </label>
+                                    </div>
+                                    <div id="left-telegram" class="selectgroup selectgroup-pills" style="display:block">
+                                        <label class="selectgroup-item">
+                                            <input id="left-cb-telegram" type="checkbox" class="selectgroup-input" onclick="LeftCBTelegram()">
+                                            <span class="selectgroup-button">Telegram</span>
+                                        </label>
+                                    </div>
+                                    <div id="left-youtube" class="selectgroup selectgroup-pills" style="display:block">
+                                        <label class="selectgroup-item">
+                                            <input id="left-cb-youtube" type="checkbox" class="selectgroup-input" onclick="LeftCBYouTube()">
+                                            <span class="selectgroup-button">YouTube</span>
                                         </label>
                                     </div>
                                 </div>
@@ -106,6 +135,42 @@
                                                 <span class="selectgroup-button">Facebook</span>
                                             </label>
                                         </div>
+                                        <div id="right-twitter" class="selectgroup selectgroup-pills" style="display:none">
+                                            <label class="selectgroup-item">
+                                                <input id="right-cb-twitter" type="checkbox" name="socmed[]" value="Twitter" class="selectgroup-input" onclick="RightCBTwitter()">
+                                                <span class="selectgroup-button">Twitter</span>
+                                            </label>
+                                        </div>
+                                        <div id="right-whatsapp" class="selectgroup selectgroup-pills" style="display:none">
+                                            <label class="selectgroup-item">
+                                                <input id="right-cb-whatsapp" type="checkbox" name="socmed[]" value="WhatsApp" class="selectgroup-input" onclick="RightCBWhatsApp()">
+                                                <span class="selectgroup-button">WhatsApp</span>
+                                            </label>
+                                        </div>
+                                        <div id="right-line" class="selectgroup selectgroup-pills" style="display:none">
+                                            <label class="selectgroup-item">
+                                                <input id="right-cb-line" type="checkbox" name="socmed[]" value="LINE" class="selectgroup-input" onclick="RightCBLINE()">
+                                                <span class="selectgroup-button">LINE</span>
+                                            </label>
+                                        </div>
+                                        <div id="right-instagram" class="selectgroup selectgroup-pills" style="display:none">
+                                            <label class="selectgroup-item">
+                                                <input id="right-cb-instagram" type="checkbox" name="socmed[]" value="Instagram" class="selectgroup-input" onclick="RightCBInstagram()">
+                                                <span class="selectgroup-button">Instagram</span>
+                                            </label>
+                                        </div>
+                                        <div id="right-telegram" class="selectgroup selectgroup-pills" style="display:none">
+                                            <label class="selectgroup-item">
+                                                <input id="right-cb-telegram" type="checkbox" name="socmed[]" value="Telegram" class="selectgroup-input" onclick="RightCBTelegram()">
+                                                <span class="selectgroup-button">Telegram</span>
+                                            </label>
+                                        </div>
+                                        <div id="right-youtube" class="selectgroup selectgroup-pills" style="display:none">
+                                            <label class="selectgroup-item">
+                                                <input id="right-cb-youtube" type="checkbox" name="socmed[]" value="YouTube" class="selectgroup-input" onclick="RightCBYouTube()">
+                                                <span class="selectgroup-button">YouTube</span>
+                                            </label>
+                                        </div>
                                     </div>
                                 </form>
                             </div>
@@ -120,55 +185,11 @@
         <div class="col-md"></div>
     </div>
 </div>
+
 @include('sweetalert::alert')
 @yield('js')
-<script>
-    function LeftCBFacebook() {
-        if ($('#left-cb-facebook').prop('checked')==true) {
-            $('#left-facebook').attr('style', 'display:none');
-            $('#left-cb-facebook').attr('checked', 'false');
-            $('#right-facebook').attr('style', 'display:block');
-            $('#right-cb-facebook').attr('checked', 'true');
-        }else{
-            $('#left-facebook').attr('style', 'display:none');
-            $('#left-cb-facebook').attr('checked', 'false');
-            $('#right-facebook').attr('style', 'display:block');
-            $('#right-cb-facebook').attr('checked', 'true');
-        }
-    }
-    function RightCBFacebook() {
-        if ($('#right-cb-facebook').prop('checked')==false) {
-            $('#left-facebook').attr('style', 'display:block');
-            $('#left-cb-facebook').attr('checked', 'false');
-            $('#right-facebook').attr('style', 'display:none');
-        }
-    }
-</script>
 
-{{-- Jaga Jaga Kalo Nanti Bakal Kenapa Kenapa. Jangan di Delete Dulu
-<div class="form-group">
-    <div class="selectgroup selectgroup-pills">
-        @php
-        $array = explode(", ", $data->socmed);
-        @endphp
-        <label class="selectgroup-item">
-            <input type="checkbox" name="socmed[]" value="Facebook" class="selectgroup-input" @if (in_array("Facebook", $array)) checked="true" @endif>
-            <span class="selectgroup-button">Facebook</span>
-        </label>
-        <label class="selectgroup-item">
-            <input type="checkbox" name="socmed[]" value="Instagram" class="selectgroup-input" @if (in_array("Instagram", $array)) checked="true" @endif>
-            <span class="selectgroup-button">Instagram</span>
-        </label>
-        <label class="selectgroup-item">
-            <input type="checkbox" name="socmed[]" value="WhatsApp" class="selectgroup-input" @if (in_array("WhatsApp", $array)) checked="true" @endif>
-            <span class="selectgroup-button">WhatsApp</span>
-        </label>
-        <label class="selectgroup-item">
-            <input type="checkbox" name="socmed[]" value="LINE" class="selectgroup-input" @if (in_array("LINE", $array)) checked="true" @endif>
-            <span class="selectgroup-button">LINE</span>
-        </label>
-    </div>
-</div> --}}
+<script src="{{asset('js/jejakdigital.js')}}"></script>
 
 </body>
 
